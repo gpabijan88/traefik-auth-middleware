bfa6db3111563568ed5bddcec357da34fd04d561
Greg Pabijan-Morawski <gpabijan@gp-ittech.eu>
2025-03-18 16:33:08 +0100

    fix: new code

---
+++ config_parser.go
@@ -0,0 +1,229 @@
+package traefik_auth_middleware
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	KeycloakURL        string `json:"url"`
+	ClientID           string `json:"client_id"`
+	ClientSecret       string `json:"client_secret"`
+	KeycloakRealm      string `json:"keycloak_realm"`
+	Scope              string `json:"scope"`
+	TokenCookieName    string `json:"token_cookie_name"`
+	UseAuthHeader      bool   `json:"use_auth_header"`
+	UserClaimName      string `json:"user_claim_name"`
+	UserHeaderName     string `json:"user_header_name"`
+	IgnorePathPrefixes string `json:"ignore_path_prefixes"`
+
+	ClientIDFile          string `json:"client_id_file"`
+	ClientSecretFile      string `json:"client_secret_file"`
+	KeycloakURLEnv        string `json:"url_env"`
+	ClientIDEnv           string `json:"client_id_env"`
+	ClientSecretEnv       string `json:"client_secret_env"`
+	KeycloakRealmEnv      string `json:"keycloak_realm_env"`
+	ScopeEnv              string `json:"scope_env"`
+	TokenCookieNameEnv    string `json:"token_cookie_name_env"`
+	UseAuthHeaderEnv      string `json:"use_auth_header_env"`
+	IgnorePathPrefixesEnv string `json:"ignore_path_prefixes_env"`
+}
+
+type keycloakAuth struct {
+	next               http.Handler
+	KeycloakURL        *url.URL
+	ClientID           string
+	ClientSecret       string
+	KeycloakRealm      string
+	Scope              string
+	TokenCookieName    string
+	UseAuthHeader      bool
+	UserClaimName      string
+	UserHeaderName     string
+	IgnorePathPrefixes []string
+}
+
+type KeycloakTokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type state struct {
+	RedirectURL string `json:"redirect_url"`
+}
+
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+func parseUrl(rawUrl string) (*url.URL, error) {
+	if rawUrl == "" {
+		return nil, errors.New("invalid empty url")
+	}
+	if !strings.Contains(rawUrl, "://") {
+		rawUrl = "https://" + rawUrl
+	}
+	u, err := url.Parse(rawUrl)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasPrefix(u.Scheme, "http") {
+		return nil, fmt.Errorf("%v is not a valid scheme", u.Scheme)
+	}
+	return u, nil
+}
+
+func readSecretFiles(config *Config) error {
+	if config.ClientIDFile != "" {
+		id, err := os.ReadFile(config.ClientIDFile)
+		if err != nil {
+			return err
+		}
+		clientId := string(id)
+		clientId = strings.TrimSpace(clientId)
+		clientId = strings.TrimSuffix(clientId, "\n")
+		config.ClientID = clientId
+	}
+	if config.ClientSecretFile != "" {
+		secret, err := os.ReadFile(config.ClientSecretFile)
+		if err != nil {
+			return err
+		}
+		clientSecret := string(secret)
+		clientSecret = strings.TrimSpace(clientSecret)
+		clientSecret = strings.TrimSuffix(clientSecret, "\n")
+		config.ClientSecret = clientSecret
+	}
+	return nil
+}
+
+func readConfigEnv(config *Config) error {
+	if config.KeycloakURLEnv != "" {
+		keycloakUrl := os.Getenv(config.KeycloakURLEnv)
+		if keycloakUrl == "" {
+			return errors.New("KeycloakURLEnv referenced but NOT set")
+		}
+		config.KeycloakURL = strings.TrimSpace(keycloakUrl)
+	}
+	if config.ClientIDEnv != "" {
+		clientId := os.Getenv(config.ClientIDEnv)
+		if clientId == "" {
+			return errors.New("ClientIDEnv referenced but NOT set")
+		}
+		config.ClientID = strings.TrimSpace(clientId)
+	}
+	if config.ClientSecretEnv != "" {
+		clientSecret := os.Getenv(config.ClientSecretEnv)
+		if clientSecret == "" {
+			return errors.New("ClientSecretEnv referenced but NOT set")
+		}
+		config.ClientSecret = strings.TrimSpace(clientSecret)
+	}
+	if config.KeycloakRealmEnv != "" {
+		keycloakRealm := os.Getenv(config.KeycloakRealmEnv)
+		if keycloakRealm == "" {
+			return errors.New("KeycloakRealmEnv referenced but NOT set")
+		}
+		config.KeycloakRealm = strings.TrimSpace(keycloakRealm)
+	}
+	if config.ScopeEnv != "" {
+		scope := os.Getenv(config.ScopeEnv)
+		if scope == "" {
+			return errors.New("ScopeEnv referenced but NOT set")
+		}
+		config.Scope = scope //Do not trim space here as it is common to use space as a separator and should be properly escaped when encoded
+	}
+	if config.TokenCookieNameEnv != "" {
+		tokenCookieName := os.Getenv(config.TokenCookieNameEnv)
+		if tokenCookieName == "" {
+			return errors.New("TokenCookieNameEnv referenced but NOT set")
+		}
+		config.TokenCookieName = strings.TrimSpace(tokenCookieName)
+	}
+	if config.UseAuthHeaderEnv != "" {
+		useAuthHeader, exists := os.LookupEnv(config.UseAuthHeaderEnv)
+		if !exists {
+			useAuthHeader = "false"
+		}
+		useAuthHeader = strings.ToLower(useAuthHeader)
+		config.UseAuthHeader = useAuthHeader == "true" || useAuthHeader == "1"
+	}
+	if config.IgnorePathPrefixesEnv != "" {
+		ignorePathPrefixes := os.Getenv(config.IgnorePathPrefixesEnv)
+		if ignorePathPrefixes == "" {
+			return errors.New("IgnorePathPrefixesEnv referenced but NOT set")
+		}
+		config.IgnorePathPrefixes = strings.TrimSpace(ignorePathPrefixes)
+	}
+	return nil
+}
+
+func New(uctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	err := readSecretFiles(config)
+	if err != nil {
+		return nil, err
+	}
+	err = readConfigEnv(config)
+	if err != nil {
+		return nil, err
+	}
+
+	if config.ClientID == "" || config.KeycloakRealm == "" {
+		return nil, errors.New("invalid configuration")
+	}
+
+	parsedURL, err := parseUrl(config.KeycloakURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if config.Scope == "" {
+		config.Scope = "openid"
+	}
+
+	tokenCookieName := "AUTH_TOKEN"
+	if config.TokenCookieName != "" {
+		tokenCookieName = config.TokenCookieName
+	}
+
+	useAuthHeader := false
+	if config.UseAuthHeader {
+		useAuthHeader = true
+	}
+
+	userClaimName := "preferred_username"
+	if config.UserClaimName != "" {
+		userClaimName = config.UserClaimName
+	}
+
+	userHeaderName := "X-Forwarded-User"
+	if config.UserHeaderName != "" {
+		userHeaderName = config.UserHeaderName
+	}
+
+	ignorePathPrefixes := []string{}
+	if config.IgnorePathPrefixes != "" {
+		ignorePathPrefixes = strings.Split(config.IgnorePathPrefixes, ",")
+	}
+
+	return &keycloakAuth{
+		next:               next,
+		KeycloakURL:        parsedURL,
+		ClientID:           config.ClientID,
+		ClientSecret:       config.ClientSecret,
+		KeycloakRealm:      config.KeycloakRealm,
+		Scope:              config.Scope,
+		TokenCookieName:    tokenCookieName,
+		UseAuthHeader:      useAuthHeader,
+		UserClaimName:      userClaimName,
+		UserHeaderName:     userHeaderName,
+		IgnorePathPrefixes: ignorePathPrefixes,
+	}, nil
+}
---
+++ main.go
@@ -0,0 +1,270 @@
+package traefik_auth_middleware
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func (k *keycloakAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	for _, substr := range k.IgnorePathPrefixes {
+		if strings.Contains(req.URL.Path, substr) {
+			k.next.ServeHTTP(rw, req)
+			return
+		}
+	}
+	cookie, err := req.Cookie("Authorization")
+	if err == nil && strings.HasPrefix(cookie.Value, "Bearer ") {
+		token := strings.TrimPrefix(cookie.Value, "Bearer ")
+		fmt.Printf("token = %+v\n", token)
+
+		ok, err := k.verifyToken(token)
+		fmt.Printf("ok = %+v\n", ok)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if !ok {
+			qry := req.URL.Query()
+			qry.Del("code")
+			qry.Del("state")
+			qry.Del("session_state")
+			req.URL.RawQuery = qry.Encode()
+			req.RequestURI = req.URL.RequestURI()
+
+			expiration := time.Now().Add(-24 * time.Hour)
+			newCookie := &http.Cookie{
+				Name:    "Authorization",
+				Value:   "",
+				Path:    "/",
+				Expires: expiration,
+				MaxAge:  -1,
+			}
+			http.SetCookie(rw, newCookie)
+
+			k.redirectToKeycloak(rw, req)
+			return
+		}
+		user, err := extractClaims(token, k.UserClaimName)
+		if err == nil {
+			req.Header.Set(k.UserHeaderName, user)
+		}
+
+		if k.UseAuthHeader {
+			// Optionally set the Bearer token to the Authorization header.
+			req.Header.Set("Authorization", "Bearer " + token)
+		}
+
+		k.next.ServeHTTP(rw, req)
+	} else {
+		authCode := req.URL.Query().Get("code")
+		if authCode == "" {
+			fmt.Printf("code is missing, redirect to keycloak\n")
+			k.redirectToKeycloak(rw, req)
+			return
+		}
+
+		stateBase64 := req.URL.Query().Get("state")
+		if stateBase64 == "" {
+			fmt.Printf("state is missing, redirect to keycloak\n")
+			k.redirectToKeycloak(rw, req)
+			return
+		}
+
+		fmt.Printf("exchange auth code called\n")
+		token, err := k.exchangeAuthCode(req, authCode, stateBase64)
+		fmt.Printf("exchange auth code finished %+v\n", token)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if k.UseAuthHeader {
+			// Optionally set the Bearer token to the Authorization header.
+			req.Header.Set("Authorization", "Bearer " + token)
+		}
+
+		authCookie := &http.Cookie{
+			Name:     "Authorization",
+			Value:    "Bearer " + token,
+			Secure:   true,
+			HttpOnly: true,
+			Path:     "/",
+			SameSite: http.SameSiteLaxMode, // Allows requests originating from sibling domains (same parent diff sub domain) to access the cookie
+		}
+
+		tokenCookie := &http.Cookie{
+			Name:     k.TokenCookieName, // Defaults to "AUTH_TOKEN"
+			Value:    token,
+			Secure:   true,
+			HttpOnly: true,
+			Path:     "/",
+			SameSite: http.SameSiteLaxMode, // Allows requests originating from sibling domains (same parent diff sub domain) to access the cookie
+		}
+
+		http.SetCookie(rw, authCookie)
+		req.AddCookie(authCookie) // Add the cookie to the request so it is present on the redirect and prevents infite loop of redirects.
+
+		// Set the token to a default/custom cookie that doesnt require trimming the Bearer prefix for common integration compatibility
+		http.SetCookie(rw, tokenCookie)
+		req.AddCookie(tokenCookie) // Add the cookie to the request so it is present on the initial redirect below.
+
+		qry := req.URL.Query()
+		qry.Del("code")
+		qry.Del("state")
+		qry.Del("session_state")
+		req.URL.RawQuery = qry.Encode()
+		req.RequestURI = req.URL.RequestURI()
+
+		scheme := req.Header.Get("X-Forwarded-Proto")
+		host := req.Header.Get("X-Forwarded-Host")
+		originalURL := fmt.Sprintf("%s://%s%s", scheme, host, req.RequestURI)
+
+		http.Redirect(rw, req, originalURL, http.StatusTemporaryRedirect)
+	}
+}
+
+func extractClaims(tokenString string, claimName string) (string, error) {
+	jwtContent := strings.Split(tokenString, ".")
+	if len(jwtContent) < 3 {
+		return "", fmt.Errorf("malformed jwt")
+	}
+
+	var jwtClaims map[string]interface{}
+	decoder := base64.StdEncoding.WithPadding(base64.NoPadding)
+
+	jwt_bytes, _ := decoder.DecodeString(jwtContent[1])
+	if err := json.Unmarshal(jwt_bytes, &jwtClaims); err != nil {
+		return "", err
+	}
+
+	if claimValue, ok := jwtClaims[claimName]; ok {
+		return fmt.Sprintf("%v", claimValue), nil
+	}
+	return "", fmt.Errorf("missing claim %s", claimName)
+}
+
+func (k *keycloakAuth) exchangeAuthCode(req *http.Request, authCode string, stateBase64 string) (string, error) {
+	stateBytes, _ := base64.StdEncoding.DecodeString(stateBase64)
+	var state state
+	err := json.Unmarshal(stateBytes, &state)
+	if err != nil {
+		return "", err
+	}
+
+	target := k.KeycloakURL.JoinPath(
+		"realms",
+		k.KeycloakRealm,
+		"protocol",
+		"openid-connect",
+		"token",
+	)
+	resp, err := http.PostForm(target.String(),
+		url.Values{
+			"grant_type":    {"authorization_code"},
+			"client_id":     {k.ClientID},
+			"client_secret": {k.ClientSecret},
+			"code":          {authCode},
+			"redirect_uri":  {state.RedirectURL},
+		})
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return "", errors.New("received bad response from Keycloak: " + string(body))
+	}
+
+	var tokenResponse KeycloakTokenResponse
+	err = json.NewDecoder(resp.Body).Decode(&tokenResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenResponse.AccessToken, nil
+}
+
+func (k *keycloakAuth) redirectToKeycloak(rw http.ResponseWriter, req *http.Request) {
+	scheme := req.Header.Get("X-Forwarded-Proto")
+	host := req.Header.Get("X-Forwarded-Host")
+	originalURL := fmt.Sprintf("%s://%s%s", scheme, host, req.RequestURI)
+
+	state := state{
+		RedirectURL: originalURL,
+	}
+
+	stateBytes, _ := json.Marshal(state)
+	stateBase64 := base64.StdEncoding.EncodeToString(stateBytes)
+
+	redirectURL := k.KeycloakURL.JoinPath(
+		"realms",
+		k.KeycloakRealm,
+		"protocol",
+		"openid-connect",
+		"auth",
+	)
+	redirectURL.RawQuery = url.Values{
+		"response_type": {"code"},
+		"client_id":     {k.ClientID},
+		"redirect_uri":  {originalURL},
+		"state":         {stateBase64},
+		"scope":				 {k.Scope},
+	}.Encode()
+
+	http.Redirect(rw, req, redirectURL.String(), http.StatusTemporaryRedirect)
+}
+
+func (k *keycloakAuth) verifyToken(token string) (bool, error) {
+	client := &http.Client{}
+
+	data := url.Values{
+		"token": {token},
+	}
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		k.KeycloakURL.JoinPath(
+			"realms",
+			k.KeycloakRealm,
+			"protocol",
+			"openid-connect",
+			"token",
+			"introspect",
+		).String(),
+		strings.NewReader(data.Encode()),
+	)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(k.ClientID, k.ClientSecret)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, nil
+	}
+
+	var introspectResponse map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&introspectResponse)
+	if err != nil {
+		return false, err
+	}
+
+	return introspectResponse["active"].(bool), nil
+}
